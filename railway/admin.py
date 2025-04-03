bcee053ff4acd11b30effc953c2360fa2345a88d
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-03 20:11:17 +0300

    register Route model in admin.py

--- railway/admin.py
+++ railway/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from railway.models import Crew, Station, TrainType
+from railway.models import Crew, Station, TrainType, Route
 
 admin.site.register(Crew)
 admin.site.register(Station)
 admin.site.register(TrainType)
+admin.site.register(Route)
