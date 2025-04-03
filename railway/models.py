b7b485a8b78fc7848ff6ead13b3b789a52729ead
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-03 20:14:18 +0300

    add Train model to models.py

--- railway/models.py
+++ railway/models.py
@@ -25,6 +25,20 @@ class TrainType(models.Model):
         return self.name
 
 
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        TrainType,
+        on_delete=models.CASCADE,
+        related_name="trains"
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Route(models.Model):
     source = models.ForeignKey(
         Station,
