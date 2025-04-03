03fc3d4667748e9b754a2d14711a9cbaf2572685
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-03 20:02:56 +0300

    register TrainType model in admin.py

--- railway/admin.py
+++ railway/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from railway.models import Crew, Station
+from railway.models import Crew, Station, TrainType
 
 admin.site.register(Crew)
 admin.site.register(Station)
+admin.site.register(TrainType)
