de91bb9d73d7fa9b24bfc945b11db784d9d05c31
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-04 14:16:41 +0300

    add CrewViewSet to views.py

--- railway/views.py
+++ railway/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status, mixins
+from railway.models import Crew, Station, Train, Route, Order, Journey, Ticket
+from railway.serializers import (
+    CrewSerializer,
+)
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
 
 # Create your views here.
