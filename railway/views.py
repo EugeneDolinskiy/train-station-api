74157dcf220675513566fd941cd134b37dd22adf
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-04 14:19:12 +0300

    add TrainViewSet to views.py

--- railway/views.py
+++ railway/views.py
@@ -31,5 +31,46 @@ class StationViewSet(viewsets.ModelViewSet):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
 
+class TrainViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Train.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TrainListSerializer
+        elif self.action == "retrieve":
+            return TrainDetailSerializer
+        elif self.action == "upload_image":
+            return TrainImageSerializer
+
+        return TrainSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            queryset = queryset.prefetch_related("train_type")
+
+        return queryset
+
+    @act(
+        methods=["POST"],
+        detail=True,
+        permission_classes=[IsAdminUser],
+        url_path="upload-image"
+    )
+    def upload_image(self, request, pk=None):
+        train = self.get_object()
+        serializer = self.get_serializer(train, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # Create your views here.
