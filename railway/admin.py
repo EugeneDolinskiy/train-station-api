2edf3cbe4c7e487c348bb8fed8b98080fe8f7e84
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-04 13:17:34 +0300

    register Journey model in admin.py

--- railway/admin.py
+++ railway/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from railway.models import Crew, Station, TrainType, Route, Train, Order
+from railway.models import Crew, Station, TrainType, Route, Train, Order, Journey
 
 admin.site.register(Crew)
 admin.site.register(Station)
@@ -8,3 +8,4 @@ admin.site.register(TrainType)
 admin.site.register(Route)
 admin.site.register(Train)
 admin.site.register(Order)
+admin.site.register(Journey)
