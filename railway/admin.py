5e30a2530cf6b66b53531489a5f1256acb0d17c0
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-04 13:17:12 +0300

    register Order model in admin.py

--- railway/admin.py
+++ railway/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from railway.models import Crew, Station, TrainType, Route, Train
+from railway.models import Crew, Station, TrainType, Route, Train, Order
 
 admin.site.register(Crew)
 admin.site.register(Station)
 admin.site.register(TrainType)
 admin.site.register(Route)
 admin.site.register(Train)
+admin.site.register(Order)
